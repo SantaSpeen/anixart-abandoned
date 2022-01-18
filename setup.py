e16babd92bcdffb1abae442919e273b1d64eaa27
SantaSpeen <SantaSpeen@gmail.com>
2022-01-19 00:13:08 +0300

    Add LICENSE

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='anixart',
+    version='0.3.0',
+    packages=['anixart'],
+    url='https://github.com/SantaSpeen/anixart',
+    license='MIT',
+    author='Maxim Khomutov',
+    author_email='santaspeen@gmail.com',
+    description='Anixart API wrapper'
+)
