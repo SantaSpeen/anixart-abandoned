94dbcc9b8f2956725325a8da7c8dd5ce4bb2b6ad
SantaSpeen <SantaSpeen@gmail.com>
2022-01-19 00:32:22 +0300

    Commit to pip

--- setup.py
+++ setup.py
@@ -1,12 +1,51 @@
+import os
+import sys
+
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+packages = ['anixart']
+
+requires = ['requests']
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+about = {}
+patch = here+'anixart/__version__.py'
+with open(patch, 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
-    name='anixart',
-    version='0.3.0',
-    packages=['anixart'],
-    url='https://github.com/SantaSpeen/anixart',
-    license='MIT',
-    author='Maxim Khomutov',
-    author_email='santaspeen@gmail.com',
-    description='Anixart API wrapper'
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=packages,
+    package_data={'': ['LICENSE', 'NOTICE']},
+    package_dir={'anixart': 'anixart'},
+    include_package_data=True,
+    install_requires=requires,
+    license=about['__license__'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/SantaSpeen/anixart/blob/master/README.md',
+        'Source': 'https://github.com/SantaSpeen/anixart',
+    },
+    python_requires=">=3.6",
 )
