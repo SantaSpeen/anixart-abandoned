869b4b6fb0d3b43984e6fec99db8fc810a17d60e
SantaSpeen <SantaSpeen@gmail.com>
2022-01-19 18:00:50 +0300

    0.3.1

--- anixart/__version__.py
+++ anixart/__version__.py
@@ -3,8 +3,8 @@
 __title__ = 'anixart'
 __description__ = 'Non-official Anixart API wrapper'
 __url__ = 'https://github.com/SantaSpeen/anixart'
-__version__ = '0.3.0'
-__build__ = 443
+__version__ = '0.3.1'
+__build__ = 445
 __author__ = 'Maxim Khomutov'
 __author_email__ = 'santaspeen@gmail.com'
 __license__ = False
