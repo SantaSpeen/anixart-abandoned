ad0af57471e8a76968453388d3cbf1e731490bc2
SantaSpeen <SantaSpeen@gmail.com>
2022-01-24 02:10:26 +0300

    Some fixes

--- anixart/__init__.py
+++ anixart/__init__.py
@@ -31,33 +31,48 @@ from .__version__ import __version__, __url__, __build__, __title__, __author__,
 from .api import AnixUserAccount, AnixAPI
 from .endpoints import AnixComment, AnixProfileVotedSort, AnixList
 
+version_secret = "65f1c508f136b74025215a95a1e551bc5261b4dfb68258465db34d7cd666464a"
+based_on = 21101018
+
 
 def check_update():
     import requests
-    secret = "65f1c508f136b74025215a95a1e551bc5261b4dfb68258465db34d7cd666464a"
     url = "http://santaspeen.ru:81/api/anixart/version.last?v=1.0"
     try:
-        r = requests.get(url, headers={"anixart-secret": secret}).json()
-        print(r)
+        r = requests.get(url, headers={"anixart-secret": version_secret}).json()
+        if r.get("error"):
+            print(r.get("error"))
+            return
+
         if not r['object']['is_last']:
+
+            from . import endpoints
+            url2 = endpoints.API_URL + endpoints.TOGGLES.format(based_on, False)
+            r2 = requests.get(url2, headers={'User-Agent': f'AnixartAPI/1.0-{based_on} ()'}).json()
+
             last_version = r['object']['last_version'][0]
             version = r['object']['version'][0]
+            last_anixart_version = r2['lastVersionCode']
             print(f"Found new version: {last_version}!!\n"
-                  f"Installed version: {version}.")
+                  f"Installed version: {version}.\n"
+                  f"Anixart App last build: {last_anixart_version}\n")
+
+            print("$ pip3 install -U anixart")
             ans = input("Do you need update Anixart API lib (y/n): ")
+
             if ans.lower() == "y":
                 print("\nUpdating Anixart API lib.\n")
                 import os
-                print("$ pip3 install -U anixart")
                 os.system("pip3 install -U anixart")
                 print("\n\n"
                       "Anixart API lib updated..\n"
                       "Reload script!\n")
                 exit(0)
+
             else:
                 print("Abort.")
         else:
             print("Your Anixart API lib has last version.")
+            del requests
     except requests.exceptions.ConnectionError:
         print("Can not connect to santaspeen api.")
-        del requests
--- anixart/__version__.py
+++ anixart/__version__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'anixart'
-__description__ = 'Non-official Anixart API wrapper'
+__description__ = 'Anixart API wrapper'
 __url__ = 'https://github.com/SantaSpeen/anixart'
 __version__ = '0.3.5.1'
-__build__ = 570
+__build__ = 572
 __author__ = 'Maxim Khomutov'
 __author_email__ = 'santaspeen@gmail.com'
 __license__ = "MIT"
